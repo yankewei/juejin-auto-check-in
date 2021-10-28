f65b77f1a6364078e6f7470549f977507f3fd38f
Kewei Yan <y947102435@gmail.com>
2021-10-28 22:24:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,24 +2,60 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"strings"
 	"os"
+	"strings"
 )
 
 func main() {
-	url := os.Getenv("FEISHU_WEBHOOK")
+	// sendAlert(checkIn())
+	sendAlert(lottery())
+}
 
+func checkIn() string {
+	url := "https://api.juejin.cn/growth_api/v1/check_in"
+	response := sendRequest(url)
 
+	type checkInRes struct {
+		ErrorNo int    `json:"err_no"`
+		ErrMsg  string `json:"err_msg"`
+	}
 
-	var str = []byte(`{"msg_type":"text", "content":"{"text":"你中奖了"}"}`)
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(str))
-	// for k, v := range getCookie() {
-	// 	req.AddCookie(&http.Cookie{Name: k, Value: v})
-	// }
+	result := &checkInRes{}
+	json.Unmarshal(response, result)
+	return result.ErrMsg
+}
+
+func lottery() string {
+	url := "https://api.juejin.cn/growth_api/v1/lottery/draw"
+	response := sendRequest(url)
+
+	type data struct {
+		Id   int    `json:"id"`
+		Name string `json:"lottery_name"`
+	}
+
+	type lotteryRes struct {
+		ErrorNo int    `json:"err_no"`
+		ErrMsg  string `json:"err_msg"`
+		Data    *data  `json:"data"`
+	}
+
+	result := &lotteryRes{}
+	json.Unmarshal(response, result)
+	return result.Data.Name
+}
+
+func sendRequest(url string) []byte {
+	req, _ := http.NewRequest("POST", url, bytes.NewBufferString("{}"))
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36")
+
+	for k, v := range getCookie() {
+		req.AddCookie(&http.Cookie{Name: k, Value: v})
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -29,7 +65,7 @@ func main() {
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
+	return body
 }
 
 func getCookie() map[string]string {
@@ -37,8 +73,35 @@ func getCookie() map[string]string {
 	slice := strings.Split(str, ";")
 	cookieMap := make(map[string]string)
 	for _, v := range slice {
-		index := strings.Index(strings.Trim(v, " "), "=")
+		v = strings.Trim(v, " ")
+		index := strings.Index(v, "=")
 		cookieMap[v[0:index]] = v[index+1:]
 	}
 	return cookieMap
+}
+
+func sendAlert(send_text string) {
+	url := os.Getenv("FEISHU_WEBHOOk")
+	type content struct {
+		Text string `json:"text"`
+	}
+
+	type parameter struct {
+		MsgType string   `json:"msg_type"`
+		Content *content `json:"content"`
+	}
+
+	str := &parameter{
+		MsgType: "text",
+		Content: &content{
+			Text: send_text,
+		},
+	}
+
+	byte, _ := json.Marshal(str)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(byte))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	client.Do(req)
 }
